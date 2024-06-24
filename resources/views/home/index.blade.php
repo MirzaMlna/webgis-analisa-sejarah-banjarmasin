d46ccbe7669af57c8a6c0f29317baa09c05ffa56
MirzaMlna <mirzamaulana713@gmail.com>
2024-06-24 12:35:55 +0800

    🐛 Modal Cant Scroll Bug

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
 
-
     <!-- Location Detail Modal -->
     <div class="modal modal-dialog modal-xl fade" id="locationModal" tabindex="-1" role="dialog"
         aria-labelledby="locationModalLabel" aria-hidden="true">
@@ -11,12 +10,11 @@
                     </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body text-start">
+                <div class="modal-body text-start" style="max-height: 70vh; overflow-y: auto;">
                     <div class="container mb-3" style="width: 50%; height: 300px;">
                         <img id="modalImage" src="" class="img-fluid" alt="Location Image"
                             style="width: 100%; height: 100%; object-fit: cover;">
                     </div>
-
                     <p><b>Koordinat : </b> <span id="modalCoordinates"></span></p>
                     <p><b>Sejarah : </b> <span id="modalHistory"></span></p>
                 </div>
@@ -26,29 +24,6 @@
             </div>
         </div>
     </div>
-
-
-    <div class="modal fade" id="locationModal" tabindex="-1" role="dialog" aria-labelledby="locationModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="locationModalLabel">Location Details</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <h2 id="modalLocationName"></h2>
-                    <p id="modalHistory"></p>
-                    <p id="modalCoordinates"></p>
-                    <img id="modalImage" src="" class="img-fluid" alt="Location Image">
-                </div>
-            </div>
-        </div>
-    </div>
-
-
 
     <x-slot:title>{{ $title }}</x-slot:title>
     @if (session('success'))
